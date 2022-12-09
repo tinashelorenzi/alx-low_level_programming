de0ca5267084495d8efefd48461426c7c80dcd82
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-09 16:23:39 +0200

    Fix indentation and betty style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,24 @@
  */
 int main(void)
 {
-  int one_zero;
-  int ones;
+	int one_zero;
+	int ones;
 
-  for (one_zero = 0; one_zero <= 9; one_zero++)
-    {
-      for (ones = one_zero + 1; ones <= 9; ones++)
+	for (one_zero = 0; one_zero <= 9; one_zero++)
 	{
-	  putchar(one_zero + '0');
-	  putchar(ones + '0');
+		for (ones = one_zero + 1; ones <= 9; ones++)
+		{
+			putchar(one_zero + '0');
+			putchar(ones + '0');
 
-	  if (one_zero < 8)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
+			if (one_zero < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-    }
-  putchar('\n');
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
