2e351b03404d76e268d55a8996f285f390efbd70
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-09 14:24:29 +0200

    1 - Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Tinashe
+*
+* Return: Always 0
+*
+*/
+int main(void)
+{
+	int alph = 'a';
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+}
