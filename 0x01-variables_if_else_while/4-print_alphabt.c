719f95a0a8b6a0a702cacefac8e609b842b84915
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-09 14:58:46 +0200

    4 - print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Tinashe
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int alph = 'a';
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+	if (alph != 'q' && alph != 'e'
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
