3474ede9dcce70a25f780f9b546121e3e7c643c3
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-09 15:07:22 +0200

    5 - print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Tinashe
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int begin = '0'
+
+	for (begin = '0'; begin <= '9'; begin++)
+	{
+		printf(begin);
+	}
+	printf("\n");
+}
