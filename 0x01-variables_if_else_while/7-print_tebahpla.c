29c9d5642b5459e5988798ef5dc3f89205db9fcb
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-09 15:19:07 +0200

    7 - Reverse Alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Tinashe
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int alph = 'z';
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+	{
+	putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
