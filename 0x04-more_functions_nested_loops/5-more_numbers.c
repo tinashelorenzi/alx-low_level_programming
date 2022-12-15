d834b46e3ecc215014f457326278b82cec8a7d8f
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-15 23:47:55 +0200

    Task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,22 +4,23 @@
 *
 *Return: returns nothing
 */
-int main(void)
+void more_numbers(void)
 {
-int number1;
-int number2;
-for (number1 = 0; number1 < 10; number1++)
-{
-for (number2 = 0; number2 <= 14; number2++)
+	int number1;
+	int number2;
 
-{
-if (number2 > 9)
-{
-_putchar((number2 / 10) + '0');
-}
-_putchar((number2 % 10) + '0');
-}
-_putchar(10);
-}
-_putchar(10);
+	for (number1 = 0; number1 < 10; number1++)
+	{
+		for (number2 = 0; number2 <= 14; number2++)
+
+		{
+			if (number2 > 9)
+			{
+				_putchar((number2 / 10) + '0');
+			}
+			_putchar((number2 % 10) + '0');
+		}
+		_putchar(10);
+	}
+	_putchar(10);
 }
