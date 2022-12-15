2f93593847c53bfe03d3cf8f02bc49269ac8a896
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-15 23:08:02 +0200

    Update config

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-*print_numbers - function that print all numbers from
-*0 to 9
+*print_most_numbers - function that print all numbers from
+*0 to 9 excluding 2 and 4
 *
 *Return: returns nothing
 */
