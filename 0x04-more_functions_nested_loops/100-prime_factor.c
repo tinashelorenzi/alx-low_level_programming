5246586183f2f52e48ea2aaea2e6e32d16de8f67
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-16 00:35:58 +0200

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include1lude <stdio.h>
+
+/**
+*main- prints the largest prime factor
+*of a number given
+*
+*Return: returns 0
+*/
+int main(void)
+{
+	long a = 612852475143;
+	int include1;
+	while (include1++ < a / 2)
+	{
+		if (a % include1 == 0)
+		{
+			a /= 2;
+			continue;
+		}
+
+		for (include1 = 3; include1 < a / 2; include1 += 2)
+		{
+			if (a % include1 == 0)
+			a /= include1;
+
+		}
+	}
+	printf("%ld\n", a);
+	return (0);
+	}
