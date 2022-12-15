62ebadff5e9259b7761784fe61aa6115c1fafcc4
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-15 22:15:22 +0200

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+	char a = 0
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('a');
+	}
+}
