4dcbbba664bb69d40bde72c02b6c888dbc317506
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-16 11:10:54 +0200

    Times table fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,28 +3,25 @@
 /**
  * times_table - prints the 9 table, starting with 0
  */
-void times_table(void)
-{
-	int num, mult, prod;
+ void times_table(void)
+ {
+ int digit, mult, result;
+ for (digit = 0; digit <= 9; digit++)
+ {
+ _putchar('0');
 
-	for (num = 0; num <= 9; num++)
-	{
-		_putchar('0');
+ for (mult = 1; mult <= 9; mult++)
+ {
+ _putchar(',');
+ _putchar(' ');
+ result = digit * mult;
+ if (result <= 9)
+ _putchar(' ');
+ else
+ _putchar((result / 10) + '0');
 
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
-	}
-}
+ _putchar((result % 10) + '0');
+ }
+ _putchar('\n');
+ }
+ }
