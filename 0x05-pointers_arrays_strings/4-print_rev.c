3e2e5cb126b6352ff9b2e70ceb01b58534163d11
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-20 22:42:56 +0200

    Fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,3 +14,15 @@ void print_rev(char *s)
 
 	_putchar('\n');
 }
+int _strlen(char *s)
+{
+	int counter, i;
+
+	counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	return (counter);
+}
