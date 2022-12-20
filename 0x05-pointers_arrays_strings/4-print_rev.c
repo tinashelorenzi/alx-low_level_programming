3af0af8fe4450f3f5a3792ca387f79cd5b2d589d
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-20 22:41:17 +0200

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_rev - Revereses a string
+* @s: string
+* Return: 0
+*/
+void print_rev(char *s);
+{
+	int len = 0, index = 0;
+	len = _strlen(s);
+
+	for (index = len - 1; index >= 0; index--)
+	_putchar(s[index]);
+
+	_putchar('\n');
+}
