e097f713c797caf237b8483691098ad4f81a8cf5
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-20 22:24:52 +0200

    Fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
-* Checks length of string
+* _strlen - Checks length of string
 * @s: string to check
 * Return: 0
 */
 int _strlen(char *s)
 {
 	int counter, i;
+	counter = 0;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		counter++;
