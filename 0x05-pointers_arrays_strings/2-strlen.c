724bc87653e3fd044f36ea774b5fd9aa0501cdd1
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-20 22:17:03 +0200

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* Checks length of string
+* @s: string to check
+* Return: 0
+*/
+int _strlen(char *s)
+{
+	int counter, i;
+	for (i = 0, s[i] != '\0', i++)
+	{
+		counter++;
+	}
+	return (counter);
+}
