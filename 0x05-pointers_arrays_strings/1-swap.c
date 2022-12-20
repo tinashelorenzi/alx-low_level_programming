19894a5ee652c56322180bcf441b6848fbb4faec
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-20 22:09:37 +0200

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*swap_int - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
+*/
+void swap_int(int *a, int *b)
+{
+	int hold = *b;
+	*b = *a;
+	*a = hold;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,5 @@
 * int prototypes()
 */
 void reset_to_98(int *n);
-
+void swap_int(int *a, int *b);
 #endif /*MAIN_H*/
