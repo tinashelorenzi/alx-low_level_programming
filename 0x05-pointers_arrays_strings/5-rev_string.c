6dd7c6fe8b254b6620ef72660b27593d8a15ca8a
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-20 22:53:24 +0200

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* rev_string - Revereses a string
+* @s: string
+* Return: 0
+*/
+void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char = word[_strlen(s)]
+
+	len = _strlen(s);
+
+	for (index = len - 1; index >= 0; index--)
+	word[index] = (s[index]);
+
+	_putchar('\n');
+}
+/**
+* _strlen - Checks length of string
+* @s: string to check
+* Return: 0
+*/
+int _strlen(char *s)
+{
+	int counter, i;
+
+	counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	return (counter);
+}
