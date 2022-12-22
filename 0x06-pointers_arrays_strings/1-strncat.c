e70e141fe52c6cbf6859e5585c745313daee62d2
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-22 19:09:54 +0200

    Task 5 fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,19 +9,15 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int length = 0;
-	int j;
 
-	while (dest[length] != '\0')
-	{
-		++length;
-	}
+	int index = 0, dest_len = 0;
 
-	for (j = 0; src[j] != '\0' && length < n - 1; ++j, ++length)
-	{
-		dest[length] = src[j];
-	}
+	while (dest[index++])
+		dest_len++;
 
-	dest[length] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
+
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,30 +6,11 @@
 */
 char *string_toupper(char *str)
 {
-	int i;
-
-	for (i = 0; i <= _strlen(str); i++)
+	for (int i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] -= 32;
 		}
 	}
-}
-/**
-* _strlen - Checks length of string
-* @s: string to check
-* Return: 0
-*/
-int _strlen(char *s)
-{
-	int counter, i;
-
-	counter = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		counter++;
-	}
-	return (counter);
 }
