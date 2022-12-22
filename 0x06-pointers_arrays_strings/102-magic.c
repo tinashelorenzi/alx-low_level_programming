2aba55dcbdf6da9760cfc48525bf15a1326b2c1f
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-23 00:40:46 +0200

    Fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
-
+/**
+* main - Changes a[5] to 98
+*
+* Return: 0
+*/
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
+  /*
+     * write your line of code here...
+     * Remember:
+     * - you are not allowed to use a
+     * - you are not allowed to modify p
+     * - only one statement
+     * - you are not allowed to code anything else than this line of code
+     */
+    /* ...so that this prints 98\n */
 
-  a[2] = 1024;
-  p = &n;
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
