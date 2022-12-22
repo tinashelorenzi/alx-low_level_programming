66212ef4162a559d1e7f1deff92651482a1ddd63
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-22 23:18:35 +0200

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,30 @@
 #include "main.h"
 /**
-* rot13 - Changes chars to rot13 encoding
-* @s: Array/string
+* print_number - Changes integers with signs
+* @n: Integer
 *
 * Return: New string
 */
 void print_number(int n)
 {
-	unassigned int num = n;
-
 	if (n < 0)
 	{
-		_putchar('-');
-		num = -num;
+		putchar('-');
+		n = -n;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	int digits[10];
+	int num_digits = 0;
 
-	_putchar((num % 10) + '0');
+	while (n > 0)
+	{
+		digits[num_digits] = n % 10;
+		n /= 10;
+		num_digits++;
+	}
+
+	for (int i = num_digits - 1; i >= 0; i--)
+	{
+		putchar(digits[i] + '0');
+	}
 }
