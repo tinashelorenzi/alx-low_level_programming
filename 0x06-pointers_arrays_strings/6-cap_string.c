06b8beec481dcc530a5983dc674e0e4b9e1e0001
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-22 19:51:37 +0200

    Fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,32 @@
 /**
 * cap_string - Changes string to uppercase
-* @s: Array/string
+* @str: Array/string
 *
 * Return: New string
 */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
 	int index = 0;
 
-while (str[++index])
-{
-	while (!(str[index] >= 'a' && str[index] <= 'z'))
-		index++;
+	while (str[++index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-	if (str[index - 1] == ' ' ||
-			str[index - 1] == '\t' ||
-			str[index - 1] == '\n' ||
-			str[index - 1] == ',' ||
-			str[index - 1] == ';' ||
-			str[index - 1] == '.' ||
-			str[index - 1] == '!' ||
-			str[index - 1] == '?' ||
-			str[index - 1] == '"' ||
-			str[index - 1] == '(' ||
-			str[index - 1] == ')' ||
-			str[index - 1] == '{' ||
-			str[index - 1] == '}')
-		str[index] -= 32;
-}
-return (str);
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			str[index] -= 32;
+	}
+	return (str);
 }
