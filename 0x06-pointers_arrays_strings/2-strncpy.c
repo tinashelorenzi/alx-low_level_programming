f2fee4a00f615cd06b017a31609fae3856510329
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2022-12-22 18:09:29 +0200

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+/**
+* _strncat - Copy strings
+* @dest: Destination
+* @src: Source
+* @n: Size of string
+*
+* Return: Mutated string @dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return dest;
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif /*MAIN_H*/
