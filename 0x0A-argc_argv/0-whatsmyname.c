f605d4796be73d686b676229d786df55767d43e0
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-09 14:43:59 +0200

    Fixes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,11 @@
 /**
 * main - prints program name
 *@argc: number of arguments
-*@argv: array of arguments
+*@argv: array of arguments type char
 *Return: 0
 */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
