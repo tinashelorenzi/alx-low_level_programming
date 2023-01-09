104a6d2a41c86b43fbd95038553986d23439d84d
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-09 14:51:20 +0200

    Fix 2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
 *@argv: array of arguments type char
 *Return: 0
 */
-int main(int argc, char char __attribute__((__unused__)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
