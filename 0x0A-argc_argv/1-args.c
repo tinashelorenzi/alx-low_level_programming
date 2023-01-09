f8fbcfe2ab846823ea3e57a0aa0e6937ac7d3f51
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-09 14:46:39 +0200

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+/**
+* main - prints argument size
+*@argc: number of arguments
+*@argv: array of arguments type char
+*Return: 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
