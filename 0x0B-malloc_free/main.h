2a9866784999b050641e8f663a37245d6e6630ee
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-10 13:51:52 +0200

    Task 0

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char)
+*/
+char *create_array(unsigned int size, char c);
+
+#endif /* MAIN_H */
