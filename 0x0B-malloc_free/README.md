4f761aa783ada29481fcc910c208af71840eab14
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-10 14:23:51 +0200

    Advanced tasks and Update README.md

--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,4 +1,12 @@
 .
+├── 0-create_array.c
+├── 100-argstostr.c
+├── 101-strtow.c
+├── 1-strdup.c
+├── 2-str_concat.c
+├── 3-alloc_grid.c
+├── 4-free_grid.c
+├── main.h
 └── README.md
 
-0 directories, 1 file
+0 directories, 9 files
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,12 @@
 *char prototypes(char)
 */
 char *create_array(unsigned int size, char c);
-
+void free_grid(int **grid, int height);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int **alloc_grid(int width, int height);
+int len(char *str);
+int num_words(char *str);
 #endif /* MAIN_H */
