c2df0ccc55ac70c781cadd94c97af79f31dcaf56
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-12 13:16:52 +0200

    Review algo

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,22 +8,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char s3[100];
-	int i = 0, j = 0;
+char *concat;
+	unsigned int len = n, i;
 
-	while (str1[i] != '\0')
-	{
-		s3[j] = s1[i];
-		i++;
-		j++;
-	}
-	i = 0;
-	while (str2[i] != '\0')
-	{
-		s3[j] = s2[i];
-		i++;
-		j++;
-	}
-	s3[j] = '\0';
-	return (s3);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i]; i++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (i = 0; s1[i]; i++)
+		concat[len++] = s1[i];
+
+	for (i = 0; s2[i] && i < n; i++)
+		concat[len++] = s2[i];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
