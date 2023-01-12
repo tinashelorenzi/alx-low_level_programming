c6041c8defb31990a54a0e2eda6655977ea67c5e
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-12 13:38:23 +0200

    Calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocate pointer of memory from array
+ * @nmemb: Array
+ * @size: Size of array
+ * Return: Null if enpty or fail or pointer to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *blk;
+
+	if (counter == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	blk = malloc(nmemb * size);
+	if (blk != NULL)
+	{
+		for (i = 0; i < (nmemb * size); i++)
+			blk[i] = 0;
+		return (blk);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
