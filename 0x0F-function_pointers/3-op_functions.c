7ab5d7164d738626f1485f0d5d0603fc7ac3a5dd
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-18 15:34:45 +0200

    Task 3 files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+/**
+ * op_sub - Subtract ints
+ * @a: First int
+ * @b: Second int
+ * Return: Answer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_add - Add ints
+ * @a: First int
+ * @b: Second int
+ * Return: Answer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_mul - Multiply ints
+ * @a: First int
+ * @b: Second int
+ * Return: Answer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Divide ints
+ * @a: First int
+ * @b: Second int
+ * Return: Answer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulo of two ints
+ * @a: First int
+ * @b: Second int
+ * Return: Answer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
