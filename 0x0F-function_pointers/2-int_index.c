960bc27f61e6bf51d210f5b8a0fdb8d716a4eda0
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-18 15:01:47 +0200

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - Compare elements of array
+ * @array: Array of elements to compare
+ * @size: Size of array
+ * @cmp: Compare function
+ * Return: Index if win, -1 if failed
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int index;
+
+	if (array && comp)
+	{
+		for (index = 0; index < size; index++)
+		{
+			if (cmp(array[index]) != 0)
+			{
+				return (index);
+			}
+		}
+	}
+	return (-1);
+}
