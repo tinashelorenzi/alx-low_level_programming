63ebd623d5c6e3b1cf11b4a7b82dd812a219abab
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-18 14:35:19 +0200

    Code fail

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,15 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int index;
 
-	if (array != NULL || size > 0 || action != NULL)
+	if (array != NULL && size > 0 && action != NULL)
 	{
 		for (index = 0; index <= size; index++)
 		{
 			action(array[index]);
 		}
+	}
+	else
+	{
+		return;
 	}
 }
