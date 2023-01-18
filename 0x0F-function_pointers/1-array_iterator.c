f2ad562b4ddc1a323bd3a8550c7a0f554e085ac1
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-18 14:39:29 +0200

    Fix 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,17 +9,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int index;
-
-	if (array != NULL && size > 0 && action != NULL)
-	{
-		for (index = 0; index <= size; index++)
-		{
-			action(array[index]);
-		}
-	}
-	else
+	if (!array || !action)
 	{
 		return;
+	}
+	unsigned int i;
+
+	for (i = 0; i <= size; i++)
+	{
+		action(array[i]);
 	}
 }
