b7277ace0206021a9a73644479d511a2bb1b840b
Tinashe <117823479+tinashelorenzi@users.noreply.github.com>
2023-01-19 12:49:49 +0000

    Betty fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,12 +8,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
+
 	if (n == 0)
 	{
 		return (0);
 	}
 	va_start(list, n);
 	int i, sum = 0;
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
