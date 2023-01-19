b7478609fa0e9c6d4f2ccad5cba2f7abd2d5f2e5
Tinashe <117823479+tinashelorenzi@users.noreply.github.com>
2023-01-19 12:48:17 +0000

    Redo Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,16 +7,17 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list list;
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list list;
 	va_start(list, n);
-	int i, total = 0;
+	int i, sum = 0;
 	for (i = 0; i < n; i++)
 	{
-		total += va_arg(list, int);
+		sum += va_arg(list, int);
 	}
 	va_end(list);
+	return (sum);
 }
