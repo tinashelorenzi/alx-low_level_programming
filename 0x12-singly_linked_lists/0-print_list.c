f3deaef3b0df20c5dd4239f8684c5a1238b4bc75
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-26 14:58:09 +0200

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "list.h"
+/**
+ * print_list - Prints the contents of linked list
+ * @list_t: Linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *ref = h;
+	size_t count = 0;
+
+	while (ref != NULL)
+	{
+		if (ref->str != NULL)
+		{
+			printf("[%d] %s\n", ref->len, ref->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+			count += 1;
+			ref = ref->next;
+		}
+	}
+
+}
