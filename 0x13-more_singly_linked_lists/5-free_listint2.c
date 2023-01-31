c872a7e8c701b5ae4581837bc7e60fe05b212b3d
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-31 13:28:33 +0200

    Task 5 - fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,19 +9,17 @@
 
 void free_listint2(listint_t **head)
 {
-listint_t *cursor;
-listint_t **temp = head;
+	listint_t *tmp;
 
-if (temp != NULL)
-{
-while (*head != NULL)
-{
-cursor = *head;
-free(cursor);
-*head = (*head)->next;
-}
+	if (head == NULL)
+		return;
 
-*temp = NULL;
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
 
-}
+	*head = NULL;
 }
