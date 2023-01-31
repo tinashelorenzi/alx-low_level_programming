effeccb308e0a0ac2e07e020cdb58c3745d2db05
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-31 12:51:36 +0200

    Update betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
 
 typedef struct listint_s
 {
